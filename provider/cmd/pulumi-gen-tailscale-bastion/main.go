989ce9a099c98705e69fd896056e1e01a12b697c
Lee Briggs <lee@leebriggs.co.uk>
2025-02-28 07:10:13 -0800

    update pulumi deps

--- provider/cmd/pulumi-gen-tailscale-bastion/main.go
+++ provider/cmd/pulumi-gen-tailscale-bastion/main.go
@@ -42,11 +42,11 @@ func emitSDK(language, outdir, schemaPath string) error {
 	var generator func() (map[string][]byte, error)
 	switch language {
 	case "dotnet":
-		generator = func() (map[string][]byte, error) { return dotnetgen.GeneratePackage(tool, pkg, extraFiles) }
+		generator = func() (map[string][]byte, error) { return dotnetgen.GeneratePackage(tool, pkg, extraFiles, map[string]string{}) }
 	case "go":
-		generator = func() (map[string][]byte, error) { return gogen.GeneratePackage(tool, pkg) }
+		generator = func() (map[string][]byte, error) { return gogen.GeneratePackage(tool, pkg, map[string]string{}) }
 	case "nodejs":
-		generator = func() (map[string][]byte, error) { return nodejsgen.GeneratePackage(tool, pkg, extraFiles) }
+		generator = func() (map[string][]byte, error) { return nodejsgen.GeneratePackage(tool, pkg, extraFiles, map[string]string{}, true) }
 	case "python":
 		generator = func() (map[string][]byte, error) { return pygen.GeneratePackage(tool, pkg, extraFiles) }
 	default:
--- provider/go.mod
+++ provider/go.mod
@@ -10,8 +10,8 @@ require (
 	github.com/pulumi/pulumi-kubernetes/sdk/v3 v3.22.1
 	github.com/pulumi/pulumi-tailscale/sdk v0.13.2
 	github.com/pulumi/pulumi-tls/sdk/v4 v4.6.1
-	github.com/pulumi/pulumi/pkg/v3 v3.91.1
-	github.com/pulumi/pulumi/sdk/v3 v3.91.1
+	github.com/pulumi/pulumi/pkg/v3 v3.153.0
+	github.com/pulumi/pulumi/sdk/v3 v3.153.0
 )
 
 require (
